81c7b0e0aae9ee4856a8585e9396dbf4e5b08c87
Tommy3235 <839264581@qq.com>
2017-09-12 21:43:40 +0800

    2017-09-12 R
    +增加了主题颜色设置功能。
    -因为一些未知的BUG，删除了百度搜索框文字提示,在未来将会再次加入这个功能。
    +白天增加了一些常用网址（详细见Github上的有关说明）。
    *从今天开始，每次更新的内容将被写入MD。

--- jsfunction.js
+++ jsfunction.js
@@ -1,4 +1,6 @@
-﻿//小齿轮设置图片更改1
+﻿// JS文件版本：12
+//-------------------------------
+//小齿轮设置图片更改1
 function onMouseMovePicture()
 {
 	var a = document.getElementById("setting_button");
@@ -49,6 +51,32 @@ function favoritePicture()
 	setCookies('bgname', a.value, 365);
 }
 
+//更改主题颜色执行部分
+function setThemeColor(co, fn)
+{
+	var al = document.getElementById("linkcss");
+	var bl = document.getElementsByClassName("setting_picture");
+	var cl = document.getElementsByClassName("importantwords");
+	al.href = fn;
+	for (var i = 0; i < b.length; i++)
+	{
+		bl[i].style.borderColor = co;
+	}
+	for (var i = 0; i < c.length; i++)
+	{
+		cl[i].style.color = co;
+	}
+}
+//更改主题颜色选择部分
+function changeThemeColor(choice)
+{
+	switch (choice)
+	{
+		case "FE4365": setThemeColor("#FE4365", "link_style_1.css");setCookies("themecolor", "FE4365", 365); break;
+		case "1E90FF": setThemeColor("#1E90FF", "link_style_2.css");setCookies("themecolor", "1E90FF", 365); break;
+		default: setThemeColor("#FE4365", "link_style_1.css"); break;
+	}
+}
 //添加Cookies
 function setCookies(name, value, time)
 {
@@ -80,6 +108,7 @@ function openHtml()
 {
 	var a = getCookies("bgname");
 	var b = document.getElementById("pb");
+	var c = getCookies("themecolor")
 	b.value = getCookies("favbgname");
 	if (a == null || a == "")
 	{
@@ -90,13 +119,26 @@ function openHtml()
 	{
 		document.body.style.backgroundImage = "url('" + a + "')";
 	}
+	if (c == null || c == "")
+	{
+		setCookies("themecolor", "FE4365", 365);
+		changeThemeColor("FE4365");
+	}
+	else
+	{
+		changeThemeColor(c);
+	}
 }
 
 //测试用函数
+function getThemeColor()
+{
+	var a = document.getElementsById("testha");
+	return a.value;
+}
 function haha()
 {
 	alert(getCookies("bgname"));
-	document.getElementById("hahaha").value = getCookies("bgname");
 }
 
 function onMouseMoveSetting(a)
@@ -104,7 +146,6 @@ function onMouseMoveSetting(a)
 	var b = document.getElementById(a);
 	b.style.borderWidth = "2px";
 	b.style.borderStyle = "solid";
-	b.style.borderColor = "#FE4365"
 }
 
 function onMouseOutSetting(a)
